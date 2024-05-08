bd75558aa1e88d86cc1884e8ba53886e8e94c753
Paprota <paprota404@gmail.com>
2024-05-08 21:22:30 +0200

    Now I need to focus on chatapp project

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,23 +7,54 @@ import {
   CardFooter,
   Divider,
   Image,
-  Button
+  Button,
 } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
 import { EyeFilledIcon } from "./EyeFilledIcon";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/popover";
+import {CircularProgress} from "@nextui-org/react";
 
 const Signup = () => {
   const [isVisible, setIsVisible] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-  const validateEmail = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+  const validateEmail = (value: string) =>
+    value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+
+  const isPasswordTooShort = (password: string) => password.length < 8;
+  const hasUppercase = (password: string) => /[A-Z]/.test(password);
+  const hasSpecialChar = (password: string) => /[^A-Za-z0-9_]/.test(password);
+
+  const validatePassword = (password: string) => {
+    if (password.length == 0) {
+      return null;
+    }
+    if (isPasswordTooShort(password)) {
+      return "Password is too short. It must be at least 8 characters long.";
+    }
+    if (!hasUppercase(password)) {
+      return "Password must contain at least one uppercase character.";
+    }
+    if (!hasSpecialChar(password)) {
+      return "Password must contain at least one special character.";
+    }
+    return null;
+  };
+
+  const passwordError = React.useMemo(() => {
+    return validatePassword(password) || "";
+  }, [password]);
+
+  const isPasswordInvalid = React.useMemo(() => {
+    return !!passwordError;
+  }, [passwordError]);
 
   const isInvalid = React.useMemo(() => {
-    if (value === "") return false;
-
-    return validateEmail(value) ? false : true;
-  }, [value]);
+    if (email === "") return false;
+    return validateEmail(email) ? false : true;
+  }, [email]);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
   return (
@@ -51,22 +82,25 @@ const Signup = () => {
         </CardHeader>
         <Divider />
         <CardBody>
-        <Input
-      value={value}
-      type="email"
-      label="Email"
-      variant="bordered"
-      isInvalid={isInvalid}
-      color={isInvalid ? "danger" : "success"}
-      errorMessage={isInvalid && "Please enter a valid email"}
-      onValueChange={setValue}
-      className="w-80"
-    />
+          <Input
+            value={email}
+            type="email"
+            label="Email"
+            variant="bordered"
+            isInvalid={isInvalid}
+            color={isInvalid ? "danger" : "success"}
+            errorMessage={isInvalid && "Please enter a valid email"}
+            onValueChange={setEmail}
+            className="w-80 pb-5"
+          />
           <Input
             label="Password"
             variant="bordered"
-            
             className="w-80"
+            isInvalid={isPasswordInvalid}
+            color={passwordError ? "danger" : "success"}
+            errorMessage={passwordError}
+            onValueChange={setPassword}
             endContent={
               <button
                 className="focus:outline-none"
@@ -85,9 +119,18 @@ const Signup = () => {
         </CardBody>
         <Divider />
         <CardFooter>
-        <Button size="md" className="bg-amber-600">
-        Sign up
-      </Button>
+          <Popover placement="bottom" showArrow={true}>
+            <PopoverTrigger>
+              <Button size="md" className="bg-amber-600">
+                Sign up
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="px-1 py-2">
+              <CircularProgress label="Signing up..." />
+              </div>
+            </PopoverContent>
+          </Popover>
         </CardFooter>
       </Card>
     </div>
